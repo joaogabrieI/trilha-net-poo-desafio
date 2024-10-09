e6b978132415228f27607c3e0a3de672e780d7f7
joaogabrieI[ <joaonalves1@gmail.com>
2024-10-09 16:18:52 -0300

    minha implementação do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("91234-5678", "modelo 1", "123456789", 8);
+Iphone iphone = new Iphone("97891-5678", "modelo 1", "123456789", 6);
+
+iphone.InstalarAplicativo("Whatsapp");
+nokia.InstalarAplicativo("Whatsapp");
